88ca78b60ca47446f795c376f668a45ab42aefb7
Vitali Bohush <bohushvitali@gmail.com>
2019-01-20 19:31:41 +0100

    update gitlab runner

---
+++ gitlab-runner-init.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+NAME="sandbox"
+
+minikube status && kubectl cluster-info || minikube start --cpus 4 --memory 8192 --disk-size "30g"
+
+wait
+
+helm init
+helm repo add gitlab https://charts.gitlab.io
+
+kubectl create clusterrolebinding gitlab-cluster-admin --clusterrole=cluster-admin --group=system:serviceaccounts --namespace=gitlab
+
+kubectl delete namespace gitlab
+
+helm del --purge gitlab-runner-$NAME
+
+helm install --namespace gitlab --name gitlab-runner-$NAME -f ./gitlab-runner-values.yml gitlab/gitlab-runner
+ 
+minikube dashboard
