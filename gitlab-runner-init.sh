f67c4d69098edad77dd8fc9fdaa5e6781bf79b1e
Vitali Bohush <bohushvitali@gmail.com>
2019-01-22 13:19:25 -0800

    update

--- gitlab-runner-init.sh
+++ gitlab-runner-init.sh
@@ -7,6 +7,7 @@ kubectl create serviceaccount gitlab-managed-apps --namespace gitlab-managed-app
 kubectl create clusterrolebinding gitlab-cluster-admin --clusterrole=cluster-admin --group=system:serviceaccounts --namespace=gitlab-managed-apps
 helm init --tiller-namespace gitlab-managed-apps
 helm repo add gitlab https://charts.gitlab.io
+
 helm install --tiller-namespace gitlab-managed-apps --namespace gitlab-managed-apps --name s3 --values minio-values.yaml stable/minio
 helm install --tiller-namespace gitlab-managed-apps --namespace gitlab-managed-apps --name runner --values gitlab-runner-values.yaml gitlab/gitlab-runner
 
