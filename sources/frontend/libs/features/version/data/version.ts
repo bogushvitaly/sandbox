12b1344985b18627573251cd10d10f8c9681b0b8
Vitali Bohush <bohushvitali@gmail.com>
2019-01-22 12:31:10 -0800

    update

--- sources/frontend/libs/features/version/data/version.ts
+++ sources/frontend/libs/features/version/data/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  name: 'frontend',
-  version: '0.0.0',
-  dirty: true,
-  raw: 'da980f5-dirty',
-  hash: 'da980f5',
-  distance: null,
-  tag: null,
-  semver: null,
-  suffix: 'da980f5-dirty',
-  semverString: null
+    "name": "frontend",
+    "version": "0.0.0",
+    "dirty": true,
+    "raw": "1dbb819-dirty",
+    "hash": "1dbb819",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "1dbb819-dirty",
+    "semverString": null
 };
 /* tslint:enable */
