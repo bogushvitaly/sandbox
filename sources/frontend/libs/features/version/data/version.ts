b2cc0493e9eb7062e5837eda6e4b1e9fccc2847a
Vitali Bohush <bohushvitali@gmail.com>
2019-01-25 04:00:48 -0800

    chore(frontend): deps update

--- sources/frontend/libs/features/version/data/version.ts
+++ sources/frontend/libs/features/version/data/version.ts
@@ -4,12 +4,12 @@ export const VERSION = {
     "name": "frontend",
     "version": "0.0.0",
     "dirty": true,
-    "raw": "f565569-dirty",
-    "hash": "f565569",
+    "raw": "061d1ac-dirty",
+    "hash": "061d1ac",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "f565569-dirty",
+    "suffix": "061d1ac-dirty",
     "semverString": null
 };
 /* tslint:enable */
