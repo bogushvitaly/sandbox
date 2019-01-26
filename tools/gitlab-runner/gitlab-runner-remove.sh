4f56595939c6064efac83a30ecd57656f5cdca07
Vitali Bohush <bohushvitali@gmail.com>
2019-01-26 02:13:39 -0800

    ci

--- tools/gitlab-runner/gitlab-runner-remove.sh
+++ tools/gitlab-runner/gitlab-runner-remove.sh
@@ -2,5 +2,6 @@
 
 PROJECT_NAME="sandbox"
 
-kubectl delete pods --namespace gitlab-managed-apps-$PROJECT_NAME -l app=minio
-kubectl delete pods --namespace gitlab-managed-apps-$PROJECT_NAME -l app=runner-gitlab-runner
+# kubectl delete pods --namespace gitlab-managed-apps-$PROJECT_NAME -l app=minio
+# kubectl delete pods --namespace gitlab-managed-apps-$PROJECT_NAME -l app=runner-gitlab-runner
+kubectl delete pods --namespace gitlab-managed-apps-$PROJECT_NAME --all
