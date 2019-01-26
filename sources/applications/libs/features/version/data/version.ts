76086c9100f0b53fc870bb19a82e5ccdd8bfa13e
Vitali Bohush <bohushvitali@gmail.com>
2019-01-26 05:18:16 -0800

    ci

--- sources/applications/libs/features/version/data/version.ts
+++ sources/applications/libs/features/version/data/version.ts
@@ -4,12 +4,12 @@ export const VERSION = {
     "name": "frontend",
     "version": "0.0.0",
     "dirty": true,
-    "raw": "3e562f7-dirty",
-    "hash": "3e562f7",
+    "raw": "753418e-dirty",
+    "hash": "753418e",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "3e562f7-dirty",
+    "suffix": "753418e-dirty",
     "semverString": null
 };
 /* tslint:enable */
