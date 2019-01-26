41f02fdb2bcbf6fc8afe18f37b6ddec5d6fec863
Vitali Bohush <bohushvitali@gmail.com>
2019-01-26 07:49:29 -0800

    fix(frontend): remove firebase functions

--- sources/applications/functions/src/index.ts
+++ sources/applications/functions/src/index.ts
@@ -3,7 +3,3 @@ import * as functions from 'firebase-functions';
 import { requestListener } from '../dist/apps/web-application/server/main';
 
 export const http = functions.https.onRequest(requestListener);
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
