d3eeb1069fc1efb812f61bee205444861750fbfc
Vitali Bohush <bohushvitali@gmail.com>
2019-01-31 03:13:54 -0800

    fix: remove canvas dev deps

--- sources/applications/libs/features/demo-version/data/version.ts
+++ sources/applications/libs/features/demo-version/data/version.ts
@@ -1,28 +1,28 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  name: 'application',
-  version: '0.0.0',
-  dirty: true,
-  raw: 'v1.0.0-14-g04302f0-dirty',
-  hash: 'g04302f0',
-  distance: 14,
-  tag: 'v1.0.0',
-  semver: {
-    options: {
-      loose: false,
-      includePrerelease: false
+    "name": "application",
+    "version": "0.0.0",
+    "dirty": true,
+    "raw": "v1.0.0-17-ga68e952-dirty",
+    "hash": "ga68e952",
+    "distance": 17,
+    "tag": "v1.0.0",
+    "semver": {
+        "options": {
+            "loose": false,
+            "includePrerelease": false
+        },
+        "loose": false,
+        "raw": "v1.0.0",
+        "major": 1,
+        "minor": 0,
+        "patch": 0,
+        "prerelease": [],
+        "build": [],
+        "version": "1.0.0"
     },
-    loose: false,
-    raw: 'v1.0.0',
-    major: 1,
-    minor: 0,
-    patch: 0,
-    prerelease: [],
-    build: [],
-    version: '1.0.0'
-  },
-  suffix: '14-g04302f0-dirty',
-  semverString: '1.0.0+14.g04302f0'
+    "suffix": "17-ga68e952-dirty",
+    "semverString": "1.0.0+17.ga68e952"
 };
 /* tslint:enable */
