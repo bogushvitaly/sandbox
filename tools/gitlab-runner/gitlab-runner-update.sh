bc6d134917322a86846b3bddd34411f8126ae4bd
Vitali Bohush <bohushvitali@gmail.com>
2019-02-05 19:57:45 +0100

    ci

--- tools/gitlab-runner/gitlab-runner-update.sh
+++ tools/gitlab-runner/gitlab-runner-update.sh
@@ -5,4 +5,3 @@ PROJECT_NAME="sandbox"
 helm upgrade --tiller-namespace gitlab-managed-apps-$PROJECT_NAME --namespace gitlab-managed-apps-$PROJECT_NAME -f minio-values.yaml s3 stable/minio
 helm upgrade --tiller-namespace gitlab-managed-apps-$PROJECT_NAME --namespace gitlab-managed-apps-$PROJECT_NAME -f gitlab-runner-values.yaml runner gitlab/gitlab-runner
 helm upgrade --tiller-namespace gitlab-managed-apps-$PROJECT_NAME --namespace gitlab-managed-apps-$PROJECT_NAME -f verdaccio-values.yaml verdaccio stable/verdaccio
- 
