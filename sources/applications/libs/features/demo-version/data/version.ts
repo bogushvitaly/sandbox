d539c6008f27e415e457cf6dc63000569233f394
Vitali Bohush <bohushvitali@gmail.com>
2019-02-05 21:45:16 +0100

    chore(apps): remove material-icons

--- sources/applications/libs/features/demo-version/data/version.ts
+++ sources/applications/libs/features/demo-version/data/version.ts
@@ -4,9 +4,9 @@ export const VERSION = {
     "name": "applications",
     "version": "0.0.0",
     "dirty": true,
-    "raw": "v1.0.0-56-gfcd9cc0-dirty",
-    "hash": "gfcd9cc0",
-    "distance": 56,
+    "raw": "v1.0.0-57-g63f46dd-dirty",
+    "hash": "g63f46dd",
+    "distance": 57,
     "tag": "v1.0.0",
     "semver": {
         "options": {
@@ -22,7 +22,7 @@ export const VERSION = {
         "build": [],
         "version": "1.0.0"
     },
-    "suffix": "56-gfcd9cc0-dirty",
-    "semverString": "1.0.0+56.gfcd9cc0"
+    "suffix": "57-g63f46dd-dirty",
+    "semverString": "1.0.0+57.g63f46dd"
 };
 /* tslint:enable */
