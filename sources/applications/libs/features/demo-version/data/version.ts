63f46ddb7ca8b5b1f3b2725ef126b56d2a658b06
Vitali Bohush <bohushvitali@gmail.com>
2019-02-05 21:32:57 +0100

    chore(apps): update deps

--- sources/applications/libs/features/demo-version/data/version.ts
+++ sources/applications/libs/features/demo-version/data/version.ts
@@ -1,28 +1,28 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  name: 'applications',
-  version: '0.0.0',
-  dirty: true,
-  raw: 'v1.0.0-44-g60ea8b3-dirty',
-  hash: 'g60ea8b3',
-  distance: 44,
-  tag: 'v1.0.0',
-  semver: {
-    options: {
-      loose: false,
-      includePrerelease: false
+    "name": "applications",
+    "version": "0.0.0",
+    "dirty": true,
+    "raw": "v1.0.0-56-gfcd9cc0-dirty",
+    "hash": "gfcd9cc0",
+    "distance": 56,
+    "tag": "v1.0.0",
+    "semver": {
+        "options": {
+            "loose": false,
+            "includePrerelease": false
+        },
+        "loose": false,
+        "raw": "v1.0.0",
+        "major": 1,
+        "minor": 0,
+        "patch": 0,
+        "prerelease": [],
+        "build": [],
+        "version": "1.0.0"
     },
-    loose: false,
-    raw: 'v1.0.0',
-    major: 1,
-    minor: 0,
-    patch: 0,
-    prerelease: [],
-    build: [],
-    version: '1.0.0'
-  },
-  suffix: '44-g60ea8b3-dirty',
-  semverString: '1.0.0+44.g60ea8b3'
+    "suffix": "56-gfcd9cc0-dirty",
+    "semverString": "1.0.0+56.gfcd9cc0"
 };
 /* tslint:enable */
