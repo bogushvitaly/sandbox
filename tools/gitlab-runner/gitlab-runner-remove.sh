3624a37cf031b95406d8b9696680e8d386a05312
Vitali Bohush <bohushvitali@gmail.com>
2019-02-05 20:41:42 +0100

    ci: update

--- tools/gitlab-runner/gitlab-runner-remove.sh
+++ tools/gitlab-runner/gitlab-runner-remove.sh
@@ -2,5 +2,10 @@
 
 PROJECT_NAME="sandbox"
 
+helm delete runner --purge --tiller-namespace gitlab-managed-apps-$PROJECT_NAME
+helm delete s3 --purge --tiller-namespace gitlab-managed-apps-$PROJECT_NAME
+helm delete verdaccio --purge --tiller-namespace gitlab-managed-apps-$PROJECT_NAME
+
+# kubectl delete pods --namespace gitlab-managed-apps-$PROJECT_NAME --all
+
 # kubectl delete pods --namespace gitlab-managed-apps-$PROJECT_NAME -l app=runner-gitlab-runner
-kubectl delete pods --namespace gitlab-managed-apps-$PROJECT_NAME --all
