df59fd1bdb34255be621010b289d915bcfc994d7
Vitali Bohush <bohushvitali@gmail.com>
2019-02-06 13:46:11 +0100

    docs(apps): change description

--- sources/applications/libs/features/demo-computer-science/classes/design-patterns/creational-builder/builder.ts
+++ sources/applications/libs/features/demo-computer-science/classes/design-patterns/creational-builder/builder.ts
@@ -1,5 +1,7 @@
 /**
- * Allows you to create different flavors of an object while avoiding constructor pollution. Useful when there could be several flavors of an object. Or when there are a lot of steps involved in creation of an object.
+ * Allows you to create different flavors of an object while avoiding constructor pollution.
+ * Useful when there could be several flavors of an object.
+ * Or when there are a lot of steps involved in creation of an object.
  */
 
 enum Size {
